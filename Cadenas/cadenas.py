98192ea5d2d06b5720e5e5633adc8b66cb0ba099
Matías Romani <33184522+matias225@users.noreply.github.com>
2026-07-02 01:14:11 -0300

    strings

---
+++ Cadenas/cadenas.py
@@ -0,0 +1,16 @@
+# 1. Comillas simples (úso común)
+fruta = 'manzana'
+
+# 2. Comillas dobles (funcionan igual)
+color = "Rojo"
+
+# 3. ¿Por qué existen las dos? Para poder poner comillas dentro del texto sin complicarse con símbolos extraños (\)
+
+# Ejemplo: Citar a alguien
+cita = 'El maestro dijo: "Estudien mucho"'
+
+# Ejemplo: Título de un libro
+libro = "Lei el libro 'El Principito' ayer"
+
+print(cita)
+print(libro)
---
+++ Cadenas/cadenas_multilinea.py
@@ -0,0 +1,23 @@
+# Usando triple comillas (""" ó ''')
+# Ideal para textos largos como menus o cartas
+
+# Ejemplo 1:
+menu = """
+MENU DEL DIA
+------------
+1. Tacos al Pastor
+2. Enchiladas Verdes
+3. Quesadillas
+"""
+print(menu)
+
+# Ejemplo 2: Correo electrónico
+coreo = '''
+Hola equipo:
+
+Les aviso que ya termine la tareade Python.
+¡El código funciona perfecto!
+
+Saludos cordiales.
+'''
+print(coreo)
