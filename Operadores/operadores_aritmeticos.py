2bf5f75c1c6ffa15c5f1fed4c00148041fdff08d
Matías Romani <33184522+matias225@users.noreply.github.com>
2026-07-05 00:35:27 -0300

    started section 6

---
+++ Operadores/operadores_aritmeticos.py
@@ -0,0 +1,31 @@
+# Operadores Aritmeticos
+a = 10
+b = 3
+
+# Suma +
+suma = a + b
+print(f"Suma: {suma}")
+
+# Resta -
+resta = a - b
+print(f"Resta: {resta}")
+
+# Multiplicación *
+multiplicar = a * b
+print(f"Multiplicación: {multiplicar}")
+
+# División /
+dividir = a / b
+print(f"División: {dividir:.2f}")
+
+# División entera (//)
+division_entera = a // b
+print(f"División entera: {division_entera}")
+
+# Módulo (%) residuo de la división
+modulo = a % b
+print(f"Residuo de la división: {modulo}")
+
+# Exponente (potencia) **
+exponente = a ** b
+print(f"Exponente: {exponente}")
---
+++ Operadores/operadores_asignacion.py
@@ -0,0 +1,15 @@
+print("*** Operadores de Asignación ***")
+numero = 5
+print(f"Valor de número: {numero}")
+numero = 10
+print(f"Valor de número: {numero}")
+cadenas = "Saludos desde Python"
+print(f"Valor de cadenas: {cadenas}")
+
+# Asignación múltiple
+x, y, z = 5, "Hola", -9.15
+print(f"Valor de x = {x}, y = {y}, z = {z}")
+
+# Asignación encadenada
+a = b = c = 10
+print(f"Valor de a = {a}, b = {b}, c = {c}")
